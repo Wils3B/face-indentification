cc56e7cb05cfd7814026c2f1f98fc3d58b3849b2
Wilson Gouanet <wilstito@gmail.com>
2021-03-11 09:56:58 -0500

    setup project with tailwind css

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './scss/index.css';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Switch>
+        <Route path="/" component={Home} />
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Home(props) {
+  return (
+    <main className="h-screen bg-green-500 flex justify-center items-center">
+      <h1 className="p-6 bg-white shadow-sm rounded-md text-2xl">Welcome to the app</h1>
+    </main>
+  )
+}
